91dc405e517c08130429eb3a296dd44ac25047ba
arthurdev <arthurmartins.dev2025@gmail.com>
2026-02-26 08:41:34 -0300

    first commit

---
+++ src/index.ts
@@ -0,0 +1,1 @@
+console.log("Hello from study-blogging-platform-api");
---
+++ vitest.config.ts
@@ -0,0 +1,21 @@
+import path from "node:path";
+import { defineConfig } from "vitest/config";
+
+export default defineConfig({
+	test: {
+		fileParallelism: false,
+		sequence: {
+			concurrent: false,
+		},
+		globals: true,
+		environment: "node",
+		coverage: {
+			provider: "v8",
+		},
+	},
+	resolve: {
+		alias: {
+			"@": path.resolve(__dirname, "./src"),
+		},
+	},
+});
