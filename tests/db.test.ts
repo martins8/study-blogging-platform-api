e5d3f780f8f2494fcf8c70709971be854f6171b7
arthurdev <arthurmartins.dev2025@gmail.com>
2026-02-26 10:33:19 -0300

    test: create initial db tests

---
+++ tests/db.test.ts
@@ -0,0 +1,44 @@
+import client from "@/db/index.js";
+
+describe("Database connection", () => {
+	it("should connect to the database successfully", async () => {
+		const result = await client.execute("SELECT 1");
+		expect(result).toBeDefined();
+		expect(result.rows).toBeDefined();
+		expect(result.rows.length).toBe(1);
+		expect(result.rows[0][0]).toBe(1);
+	});
+
+	it("should be TABLE posts exists", async () => {
+		const result = await client.execute(
+			"SELECT name FROM sqlite_master WHERE type='table' AND name='posts'",
+		);
+		expect(result).toBeDefined();
+		expect(result.rows).toBeDefined();
+		expect(result.rows.length).toBe(1);
+		expect(result.rows[0][0]).toBe("posts");
+	});
+
+	it("should be able to create a post", async () => {
+		const title = "Test Post";
+		const content = "This is a test post.";
+		const category = "Testing";
+		const tags = JSON.stringify(["test", "post"]);
+
+		await client.execute(
+			"INSERT INTO posts (title, content, category, tags) VALUES (?, ?, ?, ?)",
+			[title, content, category, tags],
+		);
+
+		const result = await client.execute("SELECT * FROM posts WHERE title = ?", [
+			title,
+		]);
+		expect(result).toBeDefined();
+		expect(result.rows).toBeDefined();
+		expect(result.rows.length).toBeGreaterThan(0);
+		expect(result.rows[0][1]).toBe(title);
+		expect(result.rows[0][2]).toBe(content);
+		expect(result.rows[0][3]).toBe(category);
+		expect(result.rows[0][4]).toBe(tags);
+	});
+});
